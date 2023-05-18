028ec586ae70ea230ddc6896caf3f5648eb887d5
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-05-18 08:25:20 +0200

    .

---
+++ dist/error.php
@@ -0,0 +1,192 @@
+<?php
+header("HTTP/1.0 404 Not Found");
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" lang="fr">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="shortcut icon" type="image/png" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+    <link rel="canonical" href="https://www.agencelapanthere.ovh/error.html">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.min.css">
+    <style>
+        @font-face {
+            font-family: 'FontAwesome-swap';
+            font-display: swap;
+            src: local('FontAwesome'), url(https:////maxcdn.bootstrapcdn.com/font-awesome/4.3.0/fonts/fontawesome-webfont.woff2?v=4.3.0) format('woff2');
+        }
+    </style>
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </noscript>
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/et-line@1.0.1/style.min.css" as="style"
+        onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/et-line@1.0.1/style.min.css">
+    </noscript>
+
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                "gtm.start": new Date().getTime(),
+                event: "gtm.js",
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, "script", "dataLayer", "GTM-K7KXNTF");
+    </script>
+    <!-- End Google Tag Manager -->
+
+
+    <title>
+        Erreur 404 - Page non trouvée | La Panthère
+    </title>
+
+    <meta name="description" content="Une erreur est survenue, la page est introuvable">
+
+    <!--START Social media descriptors-->
+    <meta property="og:description" content="Une erreur est survenue, la page est introuvable">
+    <meta property="og:title" content="Page non trouvée | La Panthère">
+    <meta property="og:url" content="https://www.agencelapanthere.ovh/error.html">
+    <meta property="og:locale" content="fr_FR">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://www.agencelapanthere.ovh/img/WebP/image-de-presentation.webp">
+    <meta property="og:site_name" content="Agence La Panthère, création de site webs & design marketing">
+    <!--Open graph END--><!--Twitter cards-->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@AgenceLaPanthère">
+    <meta name="twitter:description" content="Page non accessible ou inexistante | La Panthère">
+    <meta name="twitter:image" content="https://www.agencelapanthere.ovh/img/WebP/image-de-presentation.webp">
+    <meta name="twitter:image:alt" content="Image du logo de La Panthère">
+    <!--END Social media descriptors-->
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7KXNTF" height="0" width="0"
+            style="display: none; visibility: hidden" title="GTag Manager"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Principal conteneur -->
+    <div class="page-container">
+        <!-- bloc-0 -->
+        <!-- bloc 0.1 new header-->
+        <header class="navbar navbar-expand-lg navbar-light bgc-white">
+            <div class="container py-2">
+                <a class="navbar-brand" href="./">
+                    <img src="img/agence-la-panthere-monochrome.svg" alt="(logo) La Panthère, agence de design"
+                        height="40" width="216">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <nav class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav ms-auto">
+                        <li>
+                            <a class="nav-link" href="./">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./contact.html">Contact</a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <!-- bloc-6 -->
+
+            <section class=" bloc bg-dots-bg bg-repeat bgc-atomic-tangerine d-bloc bloc-bg-texture texture-paper"
+                id="bloc-6">
+                <div class="container bloc-lg">
+                    <div class="row py-4">
+                        <article class="col-sm-12 shadow-overlay">
+                            <h1 class="text-center hero-bloc-text tc-white ">
+                                Une erreur est survenue :/ </h1>
+                            <p class="text-center mg-lg tc-white tight-width-whitespace">
+                                Il semble que la page que vous essayez d'accéder n'existe pas ou plus.
+                        </article>
+                    </div>
+                </div>
+
+            </section>
+            <!-- bloc-6 END -->
+
+
+        </main>
+
+
+
+
+        <!-- ScrollToTop Button -->
+        <a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span
+                class="fa fa-chevron-up"></span></a>
+        <!-- ScrollToTop Button END-->
+
+        <!-- Footer - bloc-8 -->
+        <footer class="bloc bgc-atomic-tangerine d-bloc" id="bloc-8">
+            <div class="container bloc-sm">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <p class="text-center white">
+                            Agence La Panthère - Agence web basée à Lyon.
+                        </p>
+                        <div class="row row-no-gutters social">
+                            <div class="col-sm-3 p-3">
+                                <div class="text-center ">
+                                    <a class="social" href="https://twitter.com" aria-label="logo twitter"><span
+                                            class="fa fa-twitter icon-md"></span></a>
+                                </div>
+                            </div>
+                            <div class="col-sm-3 p-3">
+                                <div class="text-center">
+                                    <a class="social" href="https://facebook.com" aria-label="logo facebook"><span
+                                            class="fa fa-facebook icon-md"></span></a>
+                                </div>
+                            </div>
+                            <div class="col-sm-3 p-3">
+                                <div class="text-center">
+                                    <a class="social" href="https://dribble.com" aria-label="logo dribble"><span
+                                            class="fa fa-dribbble icon-md"></span></a>
+                                </div>
+                            </div>
+                            <div class="col-sm-3 p-3">
+                                <div class="text-center">
+                                    <a class="social" href="https://instagram.com" aria-label="logo dribble"><span
+                                            class="fa fa-instagram icon-md"></span></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+        <!-- Footer - bloc-8 END -->
+    </div>
+    <!-- Principal conteneur END -->
+</body>
+
+</html>
